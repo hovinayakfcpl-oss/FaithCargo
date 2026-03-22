4c9e1c2f782c020c9080a12e9dfaad3121e0d8ae
hovinayakfcpl-oss <ho.vinayakfcpl@gmail.com>
2026-03-22 16:21:40 +0530

    fix database_url config

--- logistics_system/settings.py
+++ logistics_system/settings.py
@@ -92,12 +92,19 @@ TEMPLATES = [
 WSGI_APPLICATION = 'logistics_system.wsgi.application'
 
 # ✅ DATABASE (POSTGRESQL - RENDER)
-DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get("DATABASE_URL", "")
-        
-    )
-}
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # PASSWORD VALIDATION
 AUTH_PASSWORD_VALIDATORS = [
