7cca2a13b4a0066ddf28db722e9b93463d8fd2ba
hovinayakfcpl-oss <ho.vinayakfcpl@gmail.com>
2026-03-31 12:45:46 +0530

    Added pincode update API

--- pincode/urls.py
+++ pincode/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from .views import get_zone
 from .views import import_pincode_csv
 urlpatterns = [
+    path("zone/<str:pincode>/", get_zone),
+    path("import/", import_pincode_csv),
 
-path("zone/<str:pincode>/",get_zone),
-path("import/", import_pincode_csv),
-
-
-
+    # 🔥 ADD THIS
+    path("update/", update_pincode),
 ]
